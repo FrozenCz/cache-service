4b791992fc9d8557919e624775db2ca0d1bcfda9
FrozenCz <info@milanknop.cz>
2023-03-29 08:46:59 +0200

    added swagger, cross env and specifiing port by parameter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger('NestApplication');
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.verbose('APP start on port ' + port);
 }
 bootstrap();
